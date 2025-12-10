75e2d3dd61275e13780b9d382ac938f6464018dc
Tolga BAYRAK <bayraktolga28@gmail.com>
2025-12-10 17:40:50 +0300

    f

--- web/next.config.ts
+++ web/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:1234/api/:path*",
+        destination: "https://api.garajmuhabbet.com/api/:path*",
         has: [
           {
             type: 'header',
